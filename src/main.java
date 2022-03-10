474b17bde1bfbb6184327534d043dcd92edb92af
Manuel Lopez <74105210+ManoloTonto1@users.noreply.github.com>
2022-03-10 15:24:01 +0100

    added the menu

--- src/main.java
+++ src/main.java
@@ -1,8 +1,68 @@
-//create main class
+import java.util.ArrayList;
+//import scanner
+import java.util.Scanner;
+
 class Main {
-    //create main method
+
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        // Start any objects now like the loading from shit.
+    
+
+        Scanner scanner = new Scanner(System.in);
+        int x = 1;
+        // start the menu Loop
+        while (x == 1) {
+            System.out.println("----------------------------------------------------");
+            System.out.println("0) Exit");
+            System.out.println("1) Lijst met examens");
+            System.out.println("2) Lijst met studenten");
+            System.out.println("3) Nieuwe student inschrijven");
+            System.out.println("4) Student verwijderen");
+            System.out.println("5) Examen afnemen");
+            System.out.println("6) Is student geslaagd voor test?");
+            System.out.println("7) Welke examens heeft student gehaald?");
+            System.out.println("8) Welke student heeft de meeste examens gehaald?");
+            System.out.println("----------------------------------------------------");
+
+            int input = scanner.nextInt();
+            switch (input) {
+                case 0:
+                    System.out.println("Goodbye");
+                    x = 0;
+                    break;
+                case 1:
+                    System.out.println("===========================");
+                    //add shit here.
+                    System.out.println("===========================");
+                    break;
+                case 2:
+                    System.out.println("===========================");
+                    //add shit here
+                    System.out.println("===========================");
+                    break;
+                case 3:
+                    
+                    break;
+                case 4:
+    
+                    break;
+                case 5:
+                    
+                    break;
+                case 6:
+                    
+                    break;
+                case 7:
+                    
+                    break;
+                case 8:
+                    
+                    break;
+            }
+
+        }
+        scanner.close();
     }
 
 }
+
