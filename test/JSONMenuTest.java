b0dd45c83d6248ae97794f6770de3c59b25b3e46
Manuel Lopez <74105210+ManoloTonto1@users.noreply.github.com>
2022-03-21 14:23:52 +0100

    IMport JUnit en refactor code to work with the changes, added the json reading and writing to the JSON class

---
+++ test/JSONMenuTest.java
@@ -0,0 +1,13 @@
+import org.junit.Assert;
+import org.junit.Assert.*;
+
+
+public class JSONMenuTest {
+
+    @test
+    public void testRead(){
+        Assert.assertEquals(1, 1);
+    }
+    
+    
+}
