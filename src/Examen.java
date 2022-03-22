e2c1a5f923282eb5ba0bc6890002f13afa918839
Manuel Lopez <74105210+ManoloTonto1@users.noreply.github.com>
2022-03-22 12:32:44 +0100

    Update Examen.java

--- src/Examen.java
+++ src/Examen.java
@@ -13,30 +13,31 @@ public class Examen {
     public Examen(String naam){
         this.naam = naam;
     }
+
     public void examen1() {
-        vragen.add(new Vraag ("Bulgarije", "Servië", "Bulgarije", "Van welk land is de hoofdstad Sofia?"));
-        vragen.add(new Vraag("Europa", "Noord-Amerika", "Noord-Amerika", "Tot welk continent hoort Groenland?"));
-        vragen.add(new Vraag("Ja", "Nee", "Ja", "Is er een land met de naam Chad?"));
-        vragen.add(new Vraag("Pangea", "Eritrea", "Pangea", "Hoe heet het supercontinent ongeveer 230 miljoen jaar geleden?"));
-        vragen.add(new Vraag("Algerije", "Turkije", "Turkije", "Waar grenst de Zwarte Zee aan?"));
-        vragen.add(new Vraag("Ja", "Nee", "Ja", "Is Vaticaanstad een eigen staat?"));
-        vragen.add(new Vraag("Zuid-Amerika", "Azië", "Zuid-Amerika", "Waar ligt de Andes gebergte?"));
-        vragen.add(new Vraag("Zuid-Amerika", "Afrika", "Zuid-Amerika", "Waar ligt het land Paraguay?"));
-        vragen.add(new Vraag("Islamabad", "Kabul", "Kabul", "Wat is de hoofdstad van Afghanistan?"));
-        vragen.add(new Vraag("Ottawa", "Ontario", "Ottawa", "Wat is de hoofdstad van Canada?"));
+        vragen.add(new Vraag ("Bulgarije", "Servië", "a", "Van welk land is de hoofdstad Sofia?"));
+        vragen.add(new Vraag("Europa", "Noord-Amerika", "b", "Tot welk continent hoort Groenland?"));
+        vragen.add(new Vraag("Ja", "Nee", "a", "Is er een land met de naam Chad?"));
+        vragen.add(new Vraag("Pangea", "Eritrea", "a", "Hoe heet het supercontinent ongeveer 230 miljoen jaar geleden?"));
+        vragen.add(new Vraag("Algerije", "Turkije", "b", "Waar grenst de Zwarte Zee aan?"));
+        vragen.add(new Vraag("Ja", "Nee", "a", "Is Vaticaanstad een eigen staat?"));
+        vragen.add(new Vraag("Zuid-Amerika", "Azië", "a", "Waar ligt de Andes gebergte?"));
+        vragen.add(new Vraag("Zuid-Amerika", "Afrika", "a", "Waar ligt het land Paraguay?"));
+        vragen.add(new Vraag("Islamabad", "Kabul", "b", "Wat is de hoofdstad van Afghanistan?"));
+        vragen.add(new Vraag("Ottawa", "Ontario", "a", "Wat is de hoofdstad van Canada?"));
 
     }
     public void examen2() {
-        vragen.add(new Vraag("Blade Runner", "Zwemmen", "Blade Runner", "Welke optie heeft meer Google searches?"));
-        vragen.add(new Vraag("Coca Cola", "Netflix", "Netflix", "Welke optie heeft meer Google searches?"));
-        vragen.add(new Vraag("Epilepsie", "Nicki Minaj", "Nicki Minaj", "Welke optie heeft meer Google searches?"));
-        vragen.add(new Vraag("Rocky", "Twerken", "Twerken", "Welke optie heeft meer Google searches?"));
-        vragen.add(new Vraag("Massage", "Wii Sports", "Massage", "Welke optie heeft meer Google searches?"));
-        vragen.add(new Vraag("Thunderbirds", "Yosemite National Park", "Yosemite National Park", "Welke optie heeft meer Google searches?"));
-        vragen.add(new Vraag("Tetris", "Charles Darwin", "Tetris", "Welke optie heeft meer Google searches?"));
-        vragen.add(new Vraag("Italië", "David Bowie", "David Bowie", "Welke optie heeft meer Google searches?"));
-        vragen.add(new Vraag("Uganda", "Rijk", "Uganda", "Welke optie heeft meer Google searches?"));
-        vragen.add(new Vraag("Het Weer", "Spa", "Het weer", "Welke optie heeft meer Google searches?"));
+        vragen.add(new Vraag("Blade Runner", "Zwemmen", " a", "Welke optie heeft meer Google searches?"));
+        vragen.add(new Vraag("Coca Cola", "Netflix", "b", "Welke optie heeft meer Google searches?"));
+        vragen.add(new Vraag("Epilepsie", "Nicki Minaj", "b", "Welke optie heeft meer Google searches?"));
+        vragen.add(new Vraag("Rocky", "Twerken", "b", "Welke optie heeft meer Google searches?"));
+        vragen.add(new Vraag("Massage", "Wii Sports", "a", "Welke optie heeft meer Google searches?"));
+        vragen.add(new Vraag("Thunderbirds", "Yosemite National Park", "b", "Welke optie heeft meer Google searches?"));
+        vragen.add(new Vraag("Tetris", "Charles Darwin", "a", "Welke optie heeft meer Google searches?"));
+        vragen.add(new Vraag("Italië", "David Bowie", "b", "Welke optie heeft meer Google searches?"));
+        vragen.add(new Vraag("Uganda", "Rijk", "a", "Welke optie heeft meer Google searches?"));
+        vragen.add(new Vraag("Het Weer", "Spa", "a", "Welke optie heeft meer Google searches?"));
 
     }
     //Getters
