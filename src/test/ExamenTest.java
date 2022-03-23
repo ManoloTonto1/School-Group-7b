162a2a1336406ac645b5cecc92ce8678d6a73225
waweewawee <wilmsakasha@gmail.com>
2022-03-23 11:12:51 +0100

    JUnit test class Examen, Student en Vraag
    JUnit test class Examen, Student en Vraag

---
+++ src/test/ExamenTest.java
@@ -0,0 +1,21 @@
+package test;
+
+import static org.junit.Assert.*;
+
+import app.Examen;
+import app.Vraag;
+import org.junit.Assert;
+import org.junit.Test;
+import java.util.ArrayList;
+
+public class ExamenTest {
+    @Test
+    public void testExamen() {
+        Examen examenTest = new Examen("Topo Toets", 6);
+        ArrayList<Vraag> vraagtest = new ArrayList<Vraag>();
+        vraagtest.add(new Vraag ("Bulgarije", "Servië", "a", "Van welk land is de hoofdstad Sofia?"));
+        assertEquals("Topo Toets", examenTest.getNaam());
+        assertEquals(6, examenTest.getMinCorrect());
+        assertEquals(vraagtest.get(0),examenTest.getVragen().get(0));
+    }
+}
